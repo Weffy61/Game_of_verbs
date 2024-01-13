cc880de3af06bbc3fe9a582992d19c8c8da2645c
Weffy61 <weffy@mail.ru>
2024-01-13 19:48:39 +0300

    add DialogFlow to VK bot

--- vk_bot.py
+++ vk_bot.py
@@ -1,25 +1,44 @@
 import random
 
 from environs import Env
+from google.cloud import dialogflow
 import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 
-def echo(event, vk_api):
+def reply_message(event, vk_api, answer):
     vk_api.messages.send(
         user_id=event.user_id,
-        message=event.text,
+        message=answer,
         random_id=random.randint(1, 1000)
     )
+
+
+def detect_intent_texts(project_id, session_id, texts, language_code):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+    text_input = dialogflow.TextInput(text=texts, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+    return response.query_result.fulfillment_text
 
 
 if __name__ == "__main__":
     env = Env()
     env.read_env()
     vk_token = env.str('VK_API_KEY')
+    project_id = env.str('DIALOGFLOW_PROJECT_ID')
     vk_session = vk.VkApi(token=vk_token)
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
+            answer = detect_intent_texts(
+                project_id,
+                event.user_id,
+                event.text,
+                'ru-RU'
+            )
+            reply_message(event, vk_api, answer)
